ad2b13018eea3c9baa490efefea082ddce47db0e
trendyrituals <trendyrituals@gmail.com>
2017-10-01 23:16:13 +0530

    final touch

--- website/settings.py
+++ website/settings.py
@@ -141,7 +141,7 @@ STATICFILES_DIRS = [
     '/var/www/static/',
 ]
 
-STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_cdn")
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 
 MEDIA_URL = '/media/'
