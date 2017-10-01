b13d13346b0c23688affd9ff43a2f41639ca5857
trendyrituals <trendyrituals@gmail.com>
2017-10-01 22:44:38 +0530

    final touch

--- website/settings.py
+++ website/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '71#r*oputzc7j+8rcrbi0q)l4h!*x=%4ryu1*qr)$oylrh@@-7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
