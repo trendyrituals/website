dba213964a07c09220bd467543b76d926d14b505
trendyrituals <trendyrituals@gmail.com>
2017-10-01 23:31:34 +0530

    final touch

--- website/settings.py
+++ website/settings.py
@@ -140,5 +140,5 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL ='/media/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "media_cdn")
